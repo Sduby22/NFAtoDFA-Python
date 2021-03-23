6304d2bb1133afad1c13688c62012a5f558259fd
osennyaya <supersduby@gmail.com>
2021-03-23 21:37:46 +0800

    Add model for states. improved function printing.

--- FA.py
+++ FA.py
@@ -3,52 +3,75 @@
 from __future__ import annotations
 from typing import List, Dict
 
+class State:
+    def __init__(self, name):
+        self.name: str = name
+        self.function: Dict[str, List[State]] = {}
+    def __str__(self):
+        return self.name
+    def printFunctionString(self, isStart=0, isFinal=0):
+        string = ''
+        string += '->' if isStart else '  '
+        string += '*' if isFinal else ' '
+        string += self.name.ljust(20)
+        for x in sorted(self.function):
+            string += f'\t{str([str(y) for y in self.function[x]]).ljust(20)}' 
+        print(string)
 
 class NFA:
     def __init__(self):
-        # All the states are represented by numbers from 0 ~ (n-1)
-        self.states: List[int] = []
+        # All the states are represented by numbers string from 0 ~ (n-1)
+        self.states: List[State] = []
         self.states_num: int = 0
         self.input: List[str] = []
-        self.functions: List[Dict[str, List[int]]] = []
-        self.start_state = 0
-        self.final_states: List[int] = []
+        self.start_state: State = State(0)
+        self.final_states: List[State] = []
 
     @classmethod
     def generateFAFromFile(cls, filename: str) -> NFA:
         nfa = cls()
         with open(filename, 'r', encoding='utf-8') as f:
             nfa.states_num = int(next(f).strip())
-            nfa.states = list(range(0, nfa.states_num))
+            nfa.states = [State(str(x)) for x in range(nfa.states_num)]
             nfa.input = [x for x in next(f).strip().split(' ')]
-            nfa.functions = cls.getFunctionsFromFile(next(f).strip())
-            nfa.start_state = int(next(f).strip())
-            nfa.final_states = eval(next(f).strip())
+            funcfile = next(f).strip()
+            nfa.getFunctionsFromFile(funcfile)
+            nfa.start_state = nfa.states[int(next(f).strip())]
+            nfa.final_states = [nfa.states[x] for x in eval(next(f).strip())]
         return nfa
         
-    @classmethod
-    def getFunctionsFromFile(cls, filename: str) -> List:
-        func = []
+    def getFunctionsFromFile(self, filename: str) -> None:
         with open(filename, 'r', encoding='utf-8') as f:
-            func = eval(f.read()) 
-        return func
+            func_list = eval(f.read()) 
+            for x in range(0, self.states_num):
+                for y in func_list[x]:
+                    func_list[x][y] = [self.states[z] for z in func_list[x][y]]
+                self.states[x].function = func_list[x]
 
     def printFA(self):
         print(f'states_num: {self.states_num}')
         print(f'input: {self.input}')
-        print(f'functions:')
-        self.printFunctions()
         print(f'start_state: {self.start_state}')
-        print(f'final_states: {self.final_states}')
+        print(f'final_states: {[str(x) for x in self.final_states]}')
+        print(f'functions:')
+        self._printFunctions()
 
-    def printFunctions(self):
-        print('From\tInput\tTo')
-        for x in range(self.states_num):
-            for key, val in self.functions[x].items():
-                print(f'{x}\t{key}\t{val}')
+    def _printFunctions(self):
+        string = 'State'.ljust(20)
+        for x in sorted(self.input): 
+            string += f'\t{x.ljust(20)}'
+        print(string)
+        for x in self.states:
+            x.printFunctionString(x == self.start_state, x in self.final_states)
 
 class DFA(NFA):
     def __init__(self):
         super(NFA, self).__init__()
         self.functions: List[Dict[str, int]] = []
+
+    @classmethod
+    def genDFAFromNFA(cls, nfa: NFA) -> DFA:
+        dfa = cls()
+         
+        return dfa
 
--- test.py
+++
@@ -1,6 +0,0 @@
-
-
-str = ''
-with open('./data.in', encoding='utf8') as f:
-    for x in next(f).split():
-        print(x)
